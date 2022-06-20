0b95d22b9d08cdd54c2ff4f5fd4ffae2dec828e3
Kleber M Kardel <molinakardel@gmail.com>
2022-06-20 18:56:34 -0300

    Finaliza session products e funcionamento do carrosel através do script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+$('.card').owlCarousel({
+    margin:20,
+    loop:true,
+    autoplayTimeOut:2000,
+    autoplayHoverPauser:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false,
+        },
+        500:{
+            items:2,
+            nav:false
+        },
+        1440:{
+            items:3,
+            nav:false
+        }
+    }
+})
